f0abcfef27991c93822cb39b991d3ca38af6c16e
Daniel Beltran <genserdanie-123@hotmail.com>
2022-05-28 23:49:29 +0200

    Clase OP EJB de Sesión Local en Java EE

---
+++ sga-jee-web/src/main/java/mx/com/gm/sga/servicio/PersonaServiceImpl.java
@@ -0,0 +1,42 @@
+
+package mx.com.gm.sga.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import mx.com.gm.sga.domain.Persona;
+
+@Stateless
+public class PersonaServiceImpl implements PersonaServiceRemote, PersonaService{
+
+    @Override
+    public List<Persona> listarPersonas() {
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(1, "Juan", "Perez", "Jpserez@gmail.com", "6545113"));
+        personas.add(new Persona(2, "Marta", "Suarez", "MatSua@gmail.com", "4658122"));
+        return personas;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+    }
+    
+}
