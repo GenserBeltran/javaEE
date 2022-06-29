fbda7b31f0739f7d98977e67ac2eaf653da64cd8
Daniel Beltran <genserdanie-123@hotmail.com>
2022-06-29 13:10:55 +0200

    Clas_OP Cliente REST  - EJB con Seguridad

---
+++ ClienteSgaRestWs/src/main/java/test/PruebaRestWs.java
@@ -0,0 +1,26 @@
+package test;
+
+import domain.Persona;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+
+public class PruebaRestWs {
+    public static void main(String[] args) {
+        
+        HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().nonPreemptive().credentials("admin", "admin").build();
+        
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.register(feature);
+        
+        Client client = ClientBuilder.newClient(clientConfig);
+        
+        WebTarget webTarget = client.target("http://localhost:8080/sga-jee-web-RS/webservice").path("/personas");
+        //Proporcinando un id de persona e particular
+        Persona persona = webTarget.path("/1").request(MediaType.APPLICATION_XML).get(Persona.class);
+        System.out.println("Persona Recuperada: " + persona);
+    }
+}
